703a81bb6dc918af2e3377fbd3ae43071e7e1115
Martin Pugh <martin@pughspace.com>
2016-02-23 14:28:42 -0500

    Fix issue #1
    Fixed module not loading when specifying folder instead of PSM1 file.
    Removed automatic version incrementing.                  Updated cbh

--- Publish-Module.ps1
+++ Publish-Module.ps1
@@ -2,30 +2,19 @@
 .SYNOPSIS
     Easily build PowerShell modules for a set of functions contained in individual PS1 files
 .DESCRIPTION
-    Put a collection of your favorite functions into their own PS1 files, complete with any code you used for
-    testing purposes and this script will extract ONLY the function portion and gather all the functions into
-    a PowerShell module.  The module will be named after the folder name they're placed under.  Script will also
-    support nested folders, meaning all functions in all subfolders will be contained int he main module, but
-    a second module named after the nested folder will be created and will only contain the functions from it's
-    folder and every nested folder under there, etc.
+    Take a group of folders in this format:
+        \Source
+        \Source\Private
+        \Source\Public
 
-    Functions
-        Module1
-            Module2
-        Module3
-
-    Point the script at Functions and 3 modules will be created.  Module1 will contain all functions in the Modules1
-    and Modules 2 folder.  Module 2 will only contain the functions in the Modules2 folder.  Module3 will be a 
-    separate module and only contain the functions in that folder.
-
-    Manifest file for the module will also be created with the correct PowerShell version requirement (assuming you
-    specified this with the "#requires -Version" code in your functions), and the script will also increment the
-    version number of the module based on the number of PS1 files that have changed from the last publish.
-
-    Manifest file can also be edited to suit your requirements.
+    And create a PowerShell module from them.  Will create the folders, module file and manifest (unless it already exists).  Any 
+    PS1 file that contains a Function in it will be read and added to the module.  PS1 files in the Private folder will not be
+    publicly published in the module (this is good for helper functions) and any PS1 files in the Public folder will be available for
+    the user to run.  
 
     #Publish and #EndPublish allows you to add statements to your module.  Any code located between these lines will be included
-    in the module.  This allows the inclusion of alias' or whatever other code you need to run when the module is loaded.
+    in the module, exactly as you write them.  This allows the inclusion of alias' or whatever other code you need to run when the module 
+    is loaded.
 
     Write-Host "Not in the module"
     #Publish
@@ -39,10 +28,6 @@
 .PARAMETER Path
     The path where you module folders and PS1 files containing your functions is located.
 
-.PARAMETER ModulePath
-    The destination folder where your newly created modules and module manifest files will be stored.  Each module will
-    get it's own folder.
-
 .PARAMETER CompanyName
     Only used when a module is created for the first time, you can put the company name in the appropriate field.
 
@@ -51,10 +36,10 @@
 .OUTPUTS
     None
 .EXAMPLE
-    .\Publish-Module.ps1 -Path "\\server\share\Functions" -ModulePath "\\server\share\Modules"
+    .\Publish-Module.ps1 -Path -Path "\\server\share\Functions"
 
     Take all of the PS1 files in \\server\share\Functions and extract the functions from them and publish the resulting
-    module(s) to \\server\share\Modules, complete with manifest file.
+    module(s) in the base directory:  \\server\share\Functions
 
 .NOTES
     Author:             Martin Pugh
@@ -66,17 +51,18 @@
         1.0             Initial Release
         1.1             Added #Publish and #EndPublish support.  Any code between these lines will be included in the module.
         1.11            Publish worked, but using an Alias didn't.  Turns out modules require special handling to export the
-                        alias.  
+                        alias.
+        1.2             Fixed module not loading when specifying folder instead of PSM1 file.  Removed automatic version incrementing.
+                        Updated cbh
 .LINK
-    http://community.spiceworks.com/scripts/show/2981-create-powershell-modules-on-the-fly-publish-module-ps1
+    https://github.com/martin9700/Publish-Module
 #>
 [CmdletBinding()]
 Param (
-    [string]$Path = "c:\Dropbox\test\citrix.NetScaler"
+    [string]$Path,
+    [string]$CompanyName = "Unknown"
 )
 Write-Verbose "$(Get-Date): Publish-Module.ps1 started"
-
-$CBHKeywords = "(SYNOPSIS)|(DESCRIPTION)|(PARAMETER)|(EXAMPLE)|(INPUTS)|(OUTPUTS)|(NOTES)|(LINK)|(COMPONENT)|(ROLE)|(FUNCTIONALITY)|(FORWARDHELPTARGETNAME)|(FORWARDHELPCATEGORY)|(EXTERNALHELP)"
 
 #Test for path
 If ($Path)
@@ -104,13 +90,6 @@ ForEach ($SubPath in $PathList)
     }
     $ParentPath = $SourcePath
 }
-<#
-$HelpPath = Join-Path -Path $Path -ChildPath "en-US"
-If (-not (Test-Path $HelpPath))
-{
-    New-Item -Path $HelpPath -ItemType Directory | Out-Null
-}
-#>
 
 #Read in the functions
 $Statements = New-Object -TypeName System.Collections.ArrayList
@@ -131,7 +110,7 @@ ForEach ($FunctionType in "Private","Public")
         $Function = New-Object -TypeName System.Collections.ArrayList
         ForEach ($Line in $Raw)
         {
-            Write-host $Line
+            #Write-host $Line
             If ($Line -like "#Publish*")
             {
                 $PublishRegion = $true
@@ -176,27 +155,6 @@ ForEach ($FunctionType in "Private","Public")
             }
             If ($Begin)
             {
-                #If ($Synopsis)
-                #{
-                #    If ($Line -match $CBHKeywords -or $Line -like "*#>*")
-                <#    {
-                        $Synopsis = $false
-                    }
-                    Else
-                    {
-                        $PublicFunctionNames[$FName].Add($Line) | Out-Null
-                    }
-                }
-                If ($Line -match "\.SYNOPSIS(?<FirstLine>.*)" -and -not ($Synopsis))
-                {
-                    If ($Matches.FirstLine)
-                    {
-                        $PublicFunctionNames[$FName].Add($Matches.FirstLine) | Out-Null
-                    }
-                    $Synopsis = $true
-                }
-                #>
-
                 If ($Line -match "#requires -version (?<Version>.*)")
                 {
                     $temp = $Matches.Version + ".0"
@@ -224,84 +182,34 @@ ForEach ($FunctionType in "Private","Public")
 }
 
 #Save module
-$OutModule = Join-Path -Path $Path -ChildPath "$(Split-Path -Path $Path -Leaf).psm1"
-$Functions -join "`n`n`n" | Out-File $OutModule -Encoding ascii
-Add-Content -Value "`n`n#Included Statements:" -Path $OutModule
-Start-Sleep -Milliseconds 500
-Add-Content -Value $Statements -Path $OutModule
-Start-Sleep -Milliseconds 500
-Add-Content -Value "`n`nExport-ModuleMember -Alias * -Function `"$($PublicFunctionNames.Keys -join '","')`"" -Path $OutModule
+$ModuleName = Split-Path -Path $Path -Leaf
+$OutModule = Join-Path -Path $Path -ChildPath "$ModuleName.psm1"
+$Module = @"
+$($Functions -join "`n`n`n")
 
-<# Removing automatic help creation, it just doesn't look good
-$HelpFunctions = ForEach ($Func in $PublicFunctionNames.Keys)
-{
-    Write-Output "$Func`n"
-    ForEach ($Line in $PublicFunctionNames[$Func])
-    {
-        Write-Output "$Line`n"
-    }
-    Write-Output " "
-}
-$Help = @"
-TOPIC
-$(Split-Path -Path $Path -Leaf)
 
-LONG DESCRIPTION
-$HelpFunctions
+#Included Statements:
+$($Statements -join "`n")
+
+
+Export-ModuleMember -Alias * -Function `"$($PublicFunctionNames.Keys -join '","')`"
 "@
-$Help | Out-File (Join-Path -Path $HelpPath -ChildPath "about_$(Split-Path -Path $Path -Leaf).help.txt") -Encoding utf8
-#>
+$Module | Out-File $OutModule -Force
 
 #Create manifest
-$OutManifest = Join-Path -Path $Path -ChildPath "$(Split-Path -Path $Path -Leaf).psd1"
-If (Test-Path $OutManifest)
-{
-    $Manifest = Invoke-Expression -Command (Get-Content $OutManifest -Raw)
-    $LastChange = (Get-ChildItem $OutManifest).LastWriteTime
-    $ChangedFiles = ($Files | Where LastWriteTime -gt $LastChange).Count
-    $PercentChange = 100 - ((($Files.Count - $ChangedFiles) / $Files.Count) * 100)
-    $Version = ([version]$Manifest["ModuleVersion"]) | Select Major,Minor,Build,Revision
-    If ($PercentChange -ge 50)
-    {
-        $Version.Major ++
-        $Version.Minor = 0
-        $Version.Build = 0
-        $Version.Revision = 0
-    }
-    ElseIf ($PercentChange -ge 25)
-    {
-        $Version.Minor ++
-        $Version.Build = 0
-        $Version.Revision = 0
-    }
-    ElseIf ($PercentChagne -ge 10)
-    {
-        $Version.Build ++
-        $Version.Revision = 0
-    }
-    ElseIf ($PercentChange -gt 0)
-    {
-        $Version.Revision ++
-    }
-    $Manifest["ModuleVersion"] = "$($Version.Major).$($Version.Minor).$($Version.Build).$($Version.Revision)"
-
-}
-Else
+$OutManifest = Join-Path -Path $Path -ChildPath "$ModuleName.psd1"
+If (-not (Test-Path $OutManifest))
 {
     $Manifest = @{
-        RootModule = $Dir.BaseName
+        RootModule = $ModuleName
         ModuleVersion = "1.0.0.0"
         CompanyName = $CompanyName
+        Path = $OutManifest
+        PowerShellVersion = "$($HighVersion.Major).$($HighVersion.Minor)"
     }
+    New-ModuleManifest @Manifest
+    Write-Verbose "$(Get-Date): Manifest for $($Path.BaseName) created"
 }
-$Manifest.Add("Path",$OutManifest)
-$Manifest["PowerShellVersion"] = "$($HighVersion.Major).$($HighVersion.Minor)"
-If ($Manifest["CompanyName"] -eq "Unknown")
-{
-    $Manifest["CompanyName"] = $CompanyName
-}
-New-ModuleManifest @Manifest
-Write-Verbose "$(Get-Date): Manifest for $($Dir.BaseName) created"
 
-Write-Verbose "$(Get-Date): Modules created at: $ModulePath"
+Write-Verbose "$(Get-Date): Modules created at: $OutModule"
 Write-Verbose "$(Get-Date): Publish-Module.ps1 completed"
